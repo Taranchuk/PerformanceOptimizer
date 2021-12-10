95bc6c31aad011a0d848236ba483febdec64b446
Taranchuk <nfhfyxer@gmail.com>
2021-12-11 01:36:41 +0200

    disabled reflection cache for now

--- 1.3/Source/PerformanceOptimizer/Experimental/ReflectionCache.cs
+++ 1.3/Source/PerformanceOptimizer/Experimental/ReflectionCache.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using RimWorld;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -8,16 +9,12 @@ using System.Reflection.Emit;
 using System.Xml;
 using UnityEngine;
 using Verse;
-using Verse.Sound;
-using Verse.Steam;
-using static Mono.Security.X509.X520;
 
 namespace PerformanceOptimizer
 {
     [StaticConstructorOnStartup]
     public static class ReflectionCache
     {
-
         public static Dictionary<object, Traverse> createdTraverses = new Dictionary<object, Traverse>();
         public static Dictionary<Traverse, Dictionary<string, Traverse>> fields = new Dictionary<Traverse, Dictionary<string, Traverse>>();
         public static Dictionary<Traverse, object> fieldValues = new Dictionary<Traverse, object>();
@@ -28,12 +25,12 @@ namespace PerformanceOptimizer
             PerformanceOptimizerMod.harmony.Patch(AccessTools.Method(typeof(Traverse), "Create", new Type[] { typeof(object) }),
                 new HarmonyMethod(AccessTools.Method(typeof(ReflectionCache), nameof(TraverseCreatePrefix))),
                 new HarmonyMethod(AccessTools.Method(typeof(ReflectionCache), nameof(TraverseCreatePostfix))));
-
+            
             var fieldMethod = AccessTools.FirstMethod(typeof(Traverse), (MethodInfo mi) => mi.Name == "Field" && !mi.IsGenericMethod && mi.GetParameters().Count() == 1);
             PerformanceOptimizerMod.harmony.Patch(fieldMethod,
                 new HarmonyMethod(AccessTools.Method(typeof(ReflectionCache), nameof(TraverseFieldPrefix))),
                 new HarmonyMethod(AccessTools.Method(typeof(ReflectionCache), nameof(TraverseFieldPostfix))));
-
+            
             var getValueMethod = AccessTools.FirstMethod(typeof(Traverse), (MethodInfo mi) => mi.Name == "GetValue" && !mi.IsGenericMethod && mi.GetParameters().Count() == 0 
                 && mi.ReturnType == typeof(object));
             PerformanceOptimizerMod.harmony.Patch(getValueMethod,
@@ -41,9 +38,24 @@ namespace PerformanceOptimizer
                 new HarmonyMethod(AccessTools.Method(typeof(ReflectionCache), nameof(TraverseGetValuePostfix))));
         }
 
+        [HarmonyPatch(typeof(Pawn), nameof(Pawn.Tick))]
+        public static class Pawn_Tick_Patch
+        {
+            static void Postfix(Pawn __instance)
+            {
+                var name = __instance.nameInt;
+                var nameField = Traverse.Create(__instance).Field("nameInt").GetValue();
+                __instance.nameInt = new NameSingle("TEST");
+                var name2 = __instance.nameInt;
+                var nameField2 = Traverse.Create(__instance).Field("nameInt").GetValue();
+                Find.TickManager.CurTimeSpeed = TimeSpeed.Paused;
+                Log.Message("Before: " + name + " - " + nameField + " - after: " + name2 + " - " + nameField2);
+            }
+        }
+
         private static bool TraverseCreatePrefix(ref Traverse __result, out bool __state, object root)
         {
-            if (createdTraverses.TryGetValue(root, out __result))
+            if (root != null && createdTraverses.TryGetValue(root, out __result))
             {
                 __state = false;
                 return false;
@@ -54,7 +66,7 @@ namespace PerformanceOptimizer
 
         private static void TraverseCreatePostfix(Traverse __result, bool __state, object root)
         {
-            if (__state)
+            if (__state && root != null)
             {
                 createdTraverses[root] = __result;
             }
@@ -116,7 +128,6 @@ namespace PerformanceOptimizer
                         objectValues[__instance._root] = new Dictionary<object, object> { { __instance._info, __result } };
                     }
                 }
-
             }
         }
     }
