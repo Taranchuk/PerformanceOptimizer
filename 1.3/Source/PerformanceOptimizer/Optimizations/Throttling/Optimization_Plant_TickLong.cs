8a537ec9b43b72cf21b0d824d49dbeedc2d6a086
Taranchuk <nfhfyxer@gmail.com>
2021-12-12 11:47:18 +0200

    added plant throttling

---
+++ 1.3/Source/PerformanceOptimizer/Optimizations/Throttling/Optimization_Plant_TickLong.cs
@@ -0,0 +1,117 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using Verse;
+using Verse.AI;
+
+namespace PerformanceOptimizer
+{
+    public class Optimization_Plant_TickLong : Optimization_RefreshRate
+    {
+        public static int refreshRateStatic;
+
+        public static Dictionary<Plant, int> cachedResults = new Dictionary<Plant, int>();
+        public override int RefreshRateByDefault => 6000;
+        public override int MaxSliderValue => 10000;
+        public override OptimizationType OptimizationType => OptimizationType.Throttle;
+        public override string Name => "PO.Plant_TickLong".Translate();
+
+        public static HashSet<ThingDef> throttledPlants = new HashSet<ThingDef>();
+        public override void DoPatches()
+        {
+            base.DoPatches();
+            Patch(typeof(Plant), "TickLong", GetMethod(nameof(Prefix)), transpiler: GetMethod(nameof(Transpiler)));
+            Predicate<ThingDef> predicator = delegate (ThingDef x)
+            {
+                if (x.plant != null)
+                {
+                    if (x.comps.Count > 0)
+                    {
+                        return false;
+                    }
+                    if (x.tickerType != TickerType.Long)
+                    {
+                        return false;
+                    }
+                    if (x.plant.harvestedThingDef is null || x.plant.harvestTag == "Wood")
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            };
+            throttledPlants.AddRange(DefDatabase<ThingDef>.AllDefs.Where(x => predicator(x)));
+        }
+
+        [HarmonyPriority(Priority.First)]
+        public static bool Prefix(Plant __instance)
+        {
+            if (ShouldBeThrottled(__instance))
+            {
+                if (!cachedResults.TryGetValue(__instance, out var cache) || PerformanceOptimizerMod.tickManager.ticksGameInt >= (cache + refreshRateStatic))
+                {
+                    cachedResults[__instance] = PerformanceOptimizerMod.tickManager.ticksGameInt;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = instructions.ToList();
+            for (var i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Ldc_I4 && codes[i].OperandIs(2000))
+                {
+                    yield return new CodeInstruction(OpCodes.Ldarg_0).MoveLabelsFrom(codes[i]);
+                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Optimization_Plant_TickLong), nameof(ThrottleRateInt)));
+                }
+                else if (codes[i].opcode == OpCodes.Ldc_R4 && codes[i].OperandIs(2000f))
+                {
+                    yield return new CodeInstruction(OpCodes.Ldarg_0).MoveLabelsFrom(codes[i]);
+                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Optimization_Plant_TickLong), nameof(ThrottleRateFloat)));
+                }
+                else
+                {
+                    yield return codes[i];
+                }
+            }
+        }
+
+        public static int ThrottleRateInt(Plant plant)
+        {
+            if (ShouldBeThrottled(plant))
+            {
+                return refreshRateStatic;
+            }
+            return 2000;
+        }
+
+        public static float ThrottleRateFloat(Plant plant)
+        {
+            if (ShouldBeThrottled(plant))
+            {
+                return refreshRateStatic;
+            }
+            return 2000f;
+        }
+
+        public static bool ShouldBeThrottled(Plant plant)
+        {
+            return throttledPlants.Contains(plant.def);
+        }
+
+        public override void Clear()
+        {
+            cachedResults.Clear();
+        }
+    }
+}
