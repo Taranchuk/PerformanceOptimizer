34c77f93b4fe36e01e24c78656cdf0c4b7639c93
Taranchuk <nfhfyxer@gmail.com>
2021-12-13 04:26:48 +0200

    added GenCelestial:CurCelestialSunGlow and PlantFallColors:GetFallColorFactor throttles

---
+++ 1.3/Source/PerformanceOptimizer/Optimizations/CacheWithRefresh/Optimization_GenCelestial_CurCelestialSunGlow.cs
@@ -0,0 +1,59 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using Verse;
+
+namespace PerformanceOptimizer
+{
+    public class Optimization_GenCelestial_CurCelestialSunGlow : Optimization_RefreshRate
+    {
+        public static int refreshRateStatic;
+
+        public static Dictionary<Map, CachedValueTick<float>> cachedResults = new Dictionary<Map, CachedValueTick<float>>();
+        public override int RefreshRateByDefault => 60;
+        public override OptimizationType OptimizationType => OptimizationType.CacheWithRefreshRate;
+        public override string Name => "PO.GenCelestial_CurCelestialSunGlow".Translate();
+        public override void DoPatches()
+        {
+            base.DoPatches();
+            Patch(AccessTools.Method(typeof(GenCelestial), "CurCelestialSunGlow", new Type[] { typeof(Map) }), GetMethod(nameof(Prefix)), GetMethod(nameof(Postfix)));
+        }
+
+        [HarmonyPriority(Priority.First)]
+        public static bool Prefix(Map map, out bool __state, ref float __result)
+        {
+            if (!cachedResults.TryGetValue(map, out var cache))
+            {
+                cachedResults[map] = new CachedValueTick<float>(0, refreshRateStatic);
+                __state = true;
+                return true;
+            }
+            else if (PerformanceOptimizerMod.tickManager.ticksGameInt > cache.refreshTick)
+            {
+                __state = true;
+                return true;
+            }
+            else
+            {
+                __result = cache.valueInt;
+                __state = false;
+                return false;
+            }
+        }
+
+        [HarmonyPriority(Priority.Last)]
+        public static void Postfix(Map map, bool __state, float __result)
+        {
+            if (__state)
+            {
+                cachedResults[map].SetValue(__result, refreshRateStatic);
+            }
+        }
+
+        public override void Clear()
+        {
+            cachedResults.Clear();
+        }
+    }
+}
---
+++ 1.3/Source/PerformanceOptimizer/Optimizations/CacheWithRefresh/Optimization_PlantFallColors_GetFallColorFactor.cs
@@ -0,0 +1,61 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using Verse;
+
+namespace PerformanceOptimizer
+{
+    public class Optimization_PlantFallColors_GetFallColorFactor : Optimization_RefreshRate
+    {
+        public static int refreshRateStatic;
+        public override int RefreshRateByDefault => 4000;
+        public override OptimizationType OptimizationType => OptimizationType.CacheWithRefreshRate;
+        public override string Name => "PO.PlantFallColors_GetFallColorFactor".Translate();
+        public override void DoPatches()
+        {
+            base.DoPatches();
+            Patch(typeof(PlantFallColors), "GetFallColorFactor", GetMethod(nameof(Prefix)), GetMethod(nameof(Postfix)));
+        }
+
+        public static Dictionary<int, CachedValueTick<float>> cachedResults = new Dictionary<int, CachedValueTick<float>>();
+
+        [HarmonyPriority(Priority.First)]
+        public static bool Prefix(float latitude, int dayOfYear, out CacheData __state, ref float __result)
+        {
+            var hashcode = 23;
+            hashcode = (hashcode * 37) + latitude.GetHashCode();
+            hashcode = (hashcode * 37) + dayOfYear;
+            if (!cachedResults.TryGetValue(hashcode, out var cache))
+            {
+                cachedResults[hashcode] = new CachedValueTick<float>(0, refreshRateStatic);
+                __state = new CacheData { key = hashcode, state = true };
+                return true;
+            }
+            else if (PerformanceOptimizerMod.tickManager.ticksGameInt > cache.refreshTick)
+            {
+                __state = new CacheData { key = hashcode, state = true };
+                return true;
+            }
+            else
+            {
+                __result = cache.valueInt;
+                __state = new CacheData { key = hashcode, state = false };
+                return false;
+            }
+        }
+        [HarmonyPriority(Priority.Last)]
+        public static void Postfix(CacheData __state, float __result)
+        {
+            if (__state.state)
+            {
+                cachedResults[__state.key].SetValue(__result, refreshRateStatic);
+            }
+        }
+
+        public override void Clear()
+        {
+            cachedResults.Clear();
+        }
+    }
+}
