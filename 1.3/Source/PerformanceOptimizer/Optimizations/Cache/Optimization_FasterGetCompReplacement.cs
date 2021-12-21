9b125418f66497d06fd13270f5fe3bc10acfe503
Taranchuk <nfhfyxer@gmail.com>
2021-12-21 09:41:41 +0200

    Changed all other comp caches to Karim's code

--- 1.3/Source/PerformanceOptimizer/Optimizations/Cache/Optimization_FasterGetCompReplacement.cs
+++ 1.3/Source/PerformanceOptimizer/Optimizations/Cache/Optimization_FasterGetCompReplacement.cs
@@ -151,19 +151,19 @@ namespace PerformanceOptimizer
             catch { }
         }
 
-        public static MethodInfo genericMapGetComp = AccessTools.Method(typeof(ComponentCache), nameof(ComponentCache.GetMapComponentDict));
+        public static MethodInfo genericMapGetComp = AccessTools.Method(typeof(ComponentCache), nameof(ComponentCache.GetMapComponentFast));
         private static IEnumerable<CodeInstruction> GetMapCompTranspiler(IEnumerable<CodeInstruction> instructions)
         {
             return PerformTranspiler("GetComponent", typeof(MapComponent), genericMapGetComp, OpCodes.Callvirt, 0, instructions);
         }
 
-        public static MethodInfo genericWorldGetComp = AccessTools.Method(typeof(ComponentCache), nameof(ComponentCache.GetWorldComponentDict));
+        public static MethodInfo genericWorldGetComp = AccessTools.Method(typeof(ComponentCache), nameof(ComponentCache.GetWorldComponentFast));
         private static IEnumerable<CodeInstruction> GetWorldCompTranspiler(IEnumerable<CodeInstruction> instructions)
         {
             return PerformTranspiler("GetComponent", typeof(WorldComponent), genericWorldGetComp, OpCodes.Callvirt, 0, instructions);
         }
 
-        public static MethodInfo genericGameGetComp = AccessTools.Method(typeof(ComponentCache), nameof(ComponentCache.GetGameComponentDict));
+        public static MethodInfo genericGameGetComp = AccessTools.Method(typeof(ComponentCache), nameof(ComponentCache.GetGameComponentFast));
         private static IEnumerable<CodeInstruction> GetGameCompTranspiler(IEnumerable<CodeInstruction> instructions)
         {
             return PerformTranspiler("GetComponent", typeof(GameComponent), genericGameGetComp, OpCodes.Callvirt, 0, instructions);
@@ -199,7 +199,6 @@ namespace PerformanceOptimizer
         public override void DoPatches()
         {
             base.DoPatches();
-            Patch(typeof(ThingWithComps), "InitializeComps", postfix: GetMethod(nameof(InitializeCompsPostfix)));
             bool parse = false;
             if (methodsCallingMapGetComp is null)
             {
@@ -308,80 +307,72 @@ namespace PerformanceOptimizer
             }
         }
 
-        public static void InitializeCompsPostfix(ThingWithComps __instance)
-        {
-            if (__instance.comps != null && __instance.def.plant is null && !__instance.def.saveCompressible && __instance.comps.Count > 1)
-            {
-                __instance.comps = __instance.comps.OrderBy(delegate (ThingComp x)
-                {
-                    var index = mostCalledComps.FirstIndexOf(y => y == x.GetType().ToString());
-                    if (index == -1)
-                    {
-                        return 99999;
-                    }
-                    return index;
-                }).ToList();
-            }
-        }
-
         public override void Clear()
         {
-            ComponentCache.cachedWorldComps.Clear();
-            ComponentCache.cachedGameComps.Clear();
-            CompsOfType<Map>.mapCompsByMap.Clear();
             foreach (var type in typeof(ThingComp).AllSubclasses())
             {
-                GenGeneric.InvokeStaticMethodOnGenericType(typeof(ICache_ThingComp<>), type, "Clear");
+                try
+                {
+                    GenGeneric.InvokeStaticMethodOnGenericType(typeof(ICache_ThingComp<>), type, "Clear");
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+            foreach (var type in typeof(HediffComp).AllSubclasses())
+            {
+                try
+                {
+                    GenGeneric.InvokeStaticMethodOnGenericType(typeof(ICache_HediffComp<>), type, "Clear");
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+
+            foreach (var type in typeof(WorldObjectComp).AllSubclasses())
+            {
+                try
+                {
+                    GenGeneric.InvokeStaticMethodOnGenericType(typeof(ICache_WorldObjectComp<>), type, "Clear");
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+
+            foreach (var type in typeof(GameComponent).AllSubclasses())
+            {
+                try
+                {
+                    GenGeneric.InvokeStaticMethodOnGenericType(typeof(ICache_GameComponent<>), type, "Clear");
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+
+            foreach (var type in typeof(WorldComponent).AllSubclasses())
+            {
+                try
+                {
+                    GenGeneric.InvokeStaticMethodOnGenericType(typeof(ICache_WorldComponent<>), type, "Clear");
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+            foreach (var type in typeof(MapComponent).AllSubclasses())
+            {
+                try
+                {
+                    GenGeneric.InvokeStaticMethodOnGenericType(typeof(ICache_MapComponent<>), type, "Clear");
+                }
+                catch (Exception ex)
+                {
+                }
             }
         }
-
-        public static List<string> mostCalledComps = new List<string>
-            {
-                "RimWorld.CompQuality",
-                "Verse.CompAttachBase",
-                "Verse.CompEquippable",
-                "AlienRace.AlienPartGenerator+AlienComp",
-                "RimWorld.CompForbiddable",
-                "RimWorld.CompRottable",
-                "aRandomKiwi.GFM.Comp_Guard",
-                "RimWorld.CompBreakdownable",
-                "CaravanAdventures.CaravanStory.CompTalk",
-                "RimWorld.CompStyleable",
-                "LWM.DeepStorage.CompDeepStorage",
-                "RimWorld.CompPowerTrader",
-                "RimWorld.CompFlickable",
-                "VFECore.Abilities.CompAbilities",
-                "RimWorld.CompFacility",
-                "BestMix.CompBestMix",
-                "RimWorld.CompBiocodable",
-                "CombatExtended.CompAmmoUser",
-                "VFESecurity.CompPawnTracker",
-                "RimWorld.CompCanBeDormant",
-                "CombatExtended.CompInventory",
-                "RimWorld.CompIngredients",
-                "VFEV.Facepaint.CompFacepaint",
-                "RimWorld.CompDrug",
-                "RimWorld.CompEggLayer",
-                "RimWorld.CompAffectedByFacilities",
-                "RimWorld.CompSpawnSubplant",
-                "Verse.CompColorable",
-                "RimWorld.CompArt",
-                "PickUpAndHaul.CompHauledToInventory",
-                "CommonSense.CompUnloadChecker",
-                "Dark.Signs.Comp_Sign",
-                "Locks2.Core.LockComp",
-                "RimWorld.CompBecomeBuilding",
-                "Hospitality.CompGuest",
-                "CombatExtended.CompTacticalManager",
-                "RimWorld.CompReloadable",
-                "RimWorld.CompTransporter",
-                "DubsBadHygiene.CompBlockage",
-            };
-    }
-
-    public static class CompsOfType<T>
-    {
-        public static Dictionary<Map, T> mapCompsByMap = new Dictionary<Map, T>();
     }
 
     [StaticConstructorOnStartup]
@@ -433,36 +424,6 @@ namespace PerformanceOptimizer
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static T GetWorldObjectCompFast<T>(this WorldObject worldObject) where T : WorldObjectComp
-        {
-            //dictStopwatch.Restart();
-            if (worldObject.comps == null)
-            {
-                //dictStopwatch.LogTime("Dict approach: ");
-                return null;
-            }
-            for (int i = 0; i < worldObject.comps.Count; i++)
-            {
-                if (worldObject.comps[i].GetType() == typeof(T))
-                {
-                    //RegisterComp(thingWithComps.comps[i].GetType());
-                    //dictStopwatch.LogTime("Dict approach: ");
-                    return worldObject.comps[i] as T;
-                }
-            }
-
-            for (int i = 0; i < worldObject.comps.Count; i++)
-            {
-                T val = worldObject.comps[i] as T;
-                if (val != null)
-                {
-                    return val;
-                }
-            }
-            return null;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T TryGetThingCompFast<T>(this Thing thing) where T : ThingComp
         {
             ThingWithComps thingWithComps = thing as ThingWithComps;
@@ -473,17 +434,32 @@ namespace PerformanceOptimizer
             return thingWithComps.GetThingCompFast<T>();
         }
 
+        public static class ICache_HediffComp<T> where T : HediffComp
+        {
+            public static Dictionary<int, T> compsById = new Dictionary<int, T>();
+            public static void Clear()
+            {
+                compsById.Clear();
+            }
+        }
+
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T TryGetHediffCompFast<T>(this Hediff hd) where T : HediffComp
         {
+            if (ICache_HediffComp<T>.compsById.TryGetValue(hd.loadID, out T val))
+            {
+                return val;
+            }
             HediffWithComps hediffWithComps = hd as HediffWithComps;
             if (hediffWithComps == null)
             {
+                ICache_HediffComp<T>.compsById[hd.loadID] = null;
                 return null;
             }
             //dictStopwatch.Restart();
             if (hediffWithComps.comps == null)
             {
+                ICache_HediffComp<T>.compsById[hd.loadID] = null;
                 //dictStopwatch.LogTime("Dict approach: ");
                 return null;
             }
@@ -494,56 +470,149 @@ namespace PerformanceOptimizer
                 {
                     //RegisterComp(thingWithComps.comps[i].GetType());
                     //dictStopwatch.LogTime("Dict approach: ");
-                    return hediffWithComps.comps[i] as T;
+                    var val2 = hediffWithComps.comps[i] as T;
+                    ICache_HediffComp<T>.compsById[hd.loadID] = val2;
+                    return val2;
                 }
             }
 
             for (int i = 0; i < hediffWithComps.comps.Count; i++)
             {
-                T val = hediffWithComps.comps[i] as T;
-                if (val != null)
+                T val3 = hediffWithComps.comps[i] as T;
+                if (val3 != null)
                 {
-                    return val;
+                    ICache_HediffComp<T>.compsById[hd.loadID] = val3;
+                    return val3;
                 }
             }
+            ICache_HediffComp<T>.compsById[hd.loadID] = null;
             return null;
+        }
+        public static class ICache_WorldObjectComp<T> where T : WorldObjectComp
+        {
+            public static Dictionary<int, T> compsById = new Dictionary<int, T>();
+            public static void Clear()
+            {
+                compsById.Clear();
+            }
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T GetWorldObjectCompFast<T>(this WorldObject worldObject) where T : WorldObjectComp
+        {
+            if (ICache_WorldObjectComp<T>.compsById.TryGetValue(worldObject.ID, out T val))
+            {
+                return val;
+            }
+            //dictStopwatch.Restart();
+            if (worldObject.comps == null)
+            {
+                ICache_WorldObjectComp<T>.compsById[worldObject.ID] = null;
+                //dictStopwatch.LogTime("Dict approach: ");
+                return null;
+            }
+            for (int i = 0; i < worldObject.comps.Count; i++)
+            {
+                if (worldObject.comps[i].GetType() == typeof(T))
+                {
+                    //RegisterComp(thingWithComps.comps[i].GetType());
+                    //dictStopwatch.LogTime("Dict approach: ");
+                    var val2 = worldObject.comps[i] as T;
+                    ICache_WorldObjectComp<T>.compsById[worldObject.ID] = val2;
+                    return val2;
+                }
+            }
+
+            for (int i = 0; i < worldObject.comps.Count; i++)
+            {
+                T val3 = worldObject.comps[i] as T;
+                if (val3 != null)
+                {
+                    ICache_WorldObjectComp<T>.compsById[worldObject.ID] = val3;
+                    return val3;
+                }
+            }
+            ICache_WorldObjectComp<T>.compsById[worldObject.ID] = null;
+            return null;
+        }
+        public static class ICache_MapComponent<T>
+        {
+            public static Dictionary<Map, T> compsByMap = new Dictionary<Map, T>();
+            public static void Clear()
+            {
+                Log.Message("Clearing: " + compsByMap?.Count);
+                compsByMap.Clear();
+            }
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static T GetMapComponentDict<T>(this Map map) where T : MapComponent
+        public static T GetMapComponentFast<T>(this Map map) where T : MapComponent
         {
-            if (!CompsOfType<T>.mapCompsByMap.TryGetValue(map, out T mapComp) || mapComp is null)
+            if (!ICache_MapComponent<T>.compsByMap.TryGetValue(map, out T mapComp))
             {
-                CompsOfType<T>.mapCompsByMap[map] = mapComp = map.GetComponent<T>();
+                ICache_MapComponent<T>.compsByMap[map] = mapComp = map.GetComponent<T>();
             }
             //Log.Message("Returning map comp: " + mapComp + ", total count of map comps is " + map.components.Count);
             return mapComp;
         }
-
-        public static Dictionary<Type, WorldComponent> cachedWorldComps = new Dictionary<Type, WorldComponent>();
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static T GetWorldComponentDict<T>(this World world) where T : WorldComponent
+        public static class ICache_WorldComponent<T> where T : WorldComponent
         {
-            var type = typeof(T);
-            if (!cachedWorldComps.TryGetValue(type, out var worldComp) || worldComp is null)
+            public static World world;
+            public static T component;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static T GetComponent(World curWorld)
             {
-                cachedWorldComps[type] = worldComp = world.GetComponent<T>();
+                if (world != curWorld)
+                {
+                    world = curWorld;
+                    component = curWorld.GetComponent<T>();
+                }
+                return component;
             }
-            //Log.Message("Returning world comp: " + worldComp + ", total count of world comps is " + world.components.Count);
-            return worldComp as T;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static void Clear()
+            {
+                world = null;
+                component = null;
+            }
         }
 
-        public static Dictionary<Type, GameComponent> cachedGameComps = new Dictionary<Type, GameComponent>();
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static T GetGameComponentDict<T>(this Game game) where T : GameComponent
+        public static T GetWorldComponentFast<T>(this World world) where T : WorldComponent
         {
-            var type = typeof(T);
-            if (!cachedGameComps.TryGetValue(type, out var gameComp) || gameComp is null)
+            return ICache_WorldComponent<T>.GetComponent(world);
+        }
+        public static class ICache_GameComponent<T> where T : GameComponent
+        {
+            public static Game game;
+            public static T component;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static T GetComponent(Game curGame)
             {
-                cachedGameComps[type] = gameComp = game.GetComponent<T>();
+                if (game != curGame)
+                {
+                    game = curGame;
+                    component = curGame.GetComponent<T>();
+                }
+                return component;
             }
-            //Log.Message("Returning game comp: " + gameComp + ", total count of game comps is " + game.components.Count);
-            return gameComp as T;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static void Clear()
+            {
+                game = null;
+                component = null;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T GetGameComponentFast<T>(this Game game) where T : GameComponent
+        {
+            return ICache_GameComponent<T>.GetComponent(game);
         }
     }
 }
