9e3778df23e7ddf4292f7c47ec782242d08591f8
Taranchuk <nfhfyxer@gmail.com>
2022-02-14 17:12:42 +0200

    UI Not Included compat

--- 1.3/Source/PerformanceOptimizer/Optimizations/UITweaks/Optimization_MainButtonsRoot_DoButtons.cs
+++ 1.3/Source/PerformanceOptimizer/Optimizations/UITweaks/Optimization_MainButtonsRoot_DoButtons.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using RimWorld;
+using System.Linq;
 using UnityEngine;
 using Verse;
 
@@ -11,6 +12,35 @@ namespace PerformanceOptimizer
         {
             base.DoPatches();
             Patch(typeof(MainButtonsRoot), "DoButtons", GetMethod(nameof(Prefix)));
+            if (ModLister.AllInstalledMods.ToList().Exists(x => x.Active && x.PackageIdPlayerFacing == "GonDragon.UINotIncluded"))
+            {
+                Log.Message("[Performance Optimizer] UI Not Included detected, performing patches on it.");
+                var type = AccessTools.TypeByName("UINotIncluded.UIManager");
+                if (type is null)
+                {
+                    Log.Error("Performance Optimizer didn't found UINotIncluded.UIManager to patch. The patches were not applied.");
+                }
+                else
+                {
+                    var method1 = AccessTools.Method(type, "BarsOnGUI");
+                    if (method1 != null)
+                    {
+                        Patch(method1, GetMethod(nameof(Prefix)));
+                    }
+                    try
+                    {
+                        var method2 = AccessTools.Method(type, "VUIE_BarsOnGUI");
+                        if (method2 != null)
+                        {
+                            Patch(method2, GetMethod(nameof(Prefix)));
+                        }
+                    }
+                    catch (System.TypeLoadException)
+                    {
+
+                    }
+                }
+            }
         }
         public override string Label => "PO.HideBottomButtonBar".Translate();
 
